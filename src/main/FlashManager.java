908102f5714f0b998ab27e01af53e94426516b0d
TheComputerGeek2 <sam1010d@gmail.com>
2012-10-23 09:39:17 -0700

    The flashing stops after 5 seconds

--- src/main/FlashManager.java
+++ src/main/FlashManager.java
@@ -2,33 +2,52 @@ package main;
 
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class FlashManager implements MouseListener {
 
-	
 	private static boolean active;
-	
+
 	private static ThreePanelPicker picker;
+
+	private static Timer timer = new Timer();
+
+	public static final long DELAY = 5000;
 	
 	public FlashManager() {
-		
+
+	}
+
+	private static void setTimer() {
+		TimerTask task = new TimerTask() {
+
+			@Override
+			public void run() {
+				FlashManager.respond();
+			}
+		};
+		timer.schedule(task, FlashManager.DELAY);
 	}
 
 	public static void setActive(boolean isActive) {
 		FlashManager.active = isActive;
+		if (isActive) {
+			setTimer();
+		}
 	}
-	
+
 	public static void setDisplayManager(ThreePanelPicker picker) {
 		FlashManager.picker = picker;
 	}
-	
-	private static void respond() {
+
+	static void respond() {
 		if (active) {
 			picker.stopFlashing();
-			active= !active;
+			active = !active;
 		}
 	}
-	
+
 	@Override
 	public void mouseClicked(MouseEvent arg0) {
 		respond();
@@ -37,27 +56,25 @@ public class FlashManager implements MouseListener {
 	@Override
 	public void mouseEntered(MouseEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mouseExited(MouseEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mousePressed(MouseEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
-	
-	
 }
