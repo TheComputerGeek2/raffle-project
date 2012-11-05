b7161b00d18cdfc931d52376661e9d8f49ab0502
TheComputerGeek2 <sam1010d@gmail.com>
2012-11-05 09:52:56 -0800

    Fixed one problem and created another

--- imageDisplayDevelopment/main/TestPanel.java
+++ imageDisplayDevelopment/main/TestPanel.java
@@ -1,14 +1,16 @@
 package main;
 
+import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.GridLayout;
 import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
+import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.OverlayLayout;
 import javax.swing.SwingConstants;
 
 public class TestPanel {
@@ -27,15 +29,18 @@ public class TestPanel {
 		Graphics g = p.getGraphics();
 		g.drawImage(ImageIO.read(new File(TestPanel.IMAGE_PATH)), 0, 0, p);
 		
-		p.setLayout(new GridLayout(1,1));
+		p.setLayout(new OverlayLayout(p));
 		JPanel panel2 = new JPanel();
 		p.add(panel2);
-		panel2.setLayout(new GridLayout(1,1));
+		panel2.setLayout(new OverlayLayout(panel2));
 		JLabel l = new JLabel();
+		l.setHorizontalAlignment(SwingConstants.CENTER);
 		panel2.add(l);
-		l.setText("This is a test");
-		frame.repaint();
+		l.setText("This is a test"); //$NON-NLS-1$
+		l.repaint();
 		g.drawImage(ImageIO.read(new File(TestPanel.IMAGE_PATH)), 0, 0, p);
+		l.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		l.paintImmediately(l.getBounds());
 	}
 	
 }
