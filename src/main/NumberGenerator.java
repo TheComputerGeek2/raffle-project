fe3f10bc5bcbc0d01ab390b5e1a5b034cf87802c
TheComputerGeek2 <sam1010d@gmail.com>
2012-12-07 09:52:30 -0800

    Number generation is written but unimplemented

--- src/main/NumberGenerator.java
+++ src/main/NumberGenerator.java
@@ -3,63 +3,66 @@ package main;
 import java.util.Random;
 
 public class NumberGenerator {
-	
+
 	private Random rand;
-	
+
 	private int maximum;
-	
+
 	private int minimum;
-	
+
 	private String setValues = "";
-	
+
 	private int currentValue;
-	
+
 	private int maxLength;
-	
-public NumberGenerator() {
-	rand = new Random();
-}
 
-private void extractDigit() {
-	setValues += formatValue().charAt(setValues.length());
-	//TODO subtract the value locked from the max
-	//TODO update the maximum
-}
-
-public String getCurrent() {
-	return this.setValues + getRemainingDigits();
-}
-
-private int getRemainingDigits() {
-	//TODO determine what digits remain valid
-	return 0;
-}
-
-private String formatValue() {
-	String temp = "" + currentValue;
-	while (temp.length() < this.maxLength) {
-		temp = "0" + temp;
+	public NumberGenerator() {
+		this.rand = new Random();
 	}
-	return temp;
-	
-}
 
-public void reset() {
-	this.setValues = "";
-	this.maxLength = ("" + maximum).length();
-}
+	// TODO determine when to call this method
+	private void extractDigit() {
+		this.setValues += formatValue().charAt(this.setValues.length());
+		updateMaxValue();
+	}
 
-//TODO check that this can generate ALL values in the range
-private void generateNewValue() {
-	this.currentValue = rand.nextInt(this.maximum-this.minimum+1) + this.minimum;
-}
+	private void updateMaxValue() {
+		int unlockedValues = this.maxLength - this.setValues.length() + 1;
+		int lastLocked = Integer.parseInt(this.setValues.charAt(this.setValues
+				.length() - 1) + "");
+		this.maximum = (int) (this.maximum - (lastLocked * (Math.pow(10,
+				unlockedValues - 1))));
+	}
 
-private void setMaximum(int maximum) {
-	this.maximum = maximum;
-}
+	public String getCurrent() {
+		return this.setValues + getRemainingDigits();
+	}
 
-public void setRange(int minimum, int maximum) {
-	this.minimum = minimum;
-	this.maximum = maximum;
-}
+	private int getRemainingDigits() {
+		String temp = formatValue().substring(this.setValues.length());
+		return Integer.parseInt(temp);
+	}
+
+	private String formatValue() {
+		String temp = "" + this.currentValue;
+		while (temp.length() < this.maxLength) {
+			temp = "0" + temp;
+		}
+		return temp;
+
+	}
+
+	//TODO implement the useage of this method after verifying the range
+	// TODO check that this can generate ALL values in the range
+	private void generateNewValue() {
+		this.currentValue = this.rand.nextInt(this.maximum - this.minimum + 1)
+				+ this.minimum;
+	}
+
+	public void setRange(int minimum, int maximum) {
+		this.minimum = minimum;
+		this.maximum = maximum;
+		this.maxLength = (maximum + "").length();
+		this.setValues = "";
+	}
 }
