31332644beda9b0f2790b41c878c22d762b74fb2
TheComputerGeek2 <sam1010d@gmail.com>
2013-01-18 09:23:38 -0800

    Continued development of the help screen

--- src/main/HelpFrame.java
+++ src/main/HelpFrame.java
@@ -6,13 +6,27 @@ import javax.swing.JFrame;
 
 public class HelpFrame {
 
+	private static HelpFrame activeInstance;
+
 	private JFrame frame;
 
 	private JFrame mainFrame;
 
-	public HelpFrame(JFrame parent) {
-		this.mainFrame = parent;
+	private static final double PERCENT_OF_WIDTH = 50;
 
+	private static final double PERCENT_OF_HEIGHT = 50;
+
+	public HelpFrame(JFrame parent) {
+		if (HelpFrame.activeInstance != null) {
+			HelpFrame.activeInstance.closeFrame();
+		}
+		HelpFrame.activeInstance = this;
+		this.mainFrame = parent;
+		makeFrame();
+	}
+
+	private void closeFrame() {
+		// TODO close the help frame
 	}
 
 	private void centerOnFrame(Rectangle parent, double percentOfWidth,
@@ -28,9 +42,25 @@ public class HelpFrame {
 	}
 
 	private void centerOnFrame(Rectangle parent) {
-		// TODO center the frame on the parent while keeping the existing
-		// dimensions
+		if (this.frame.getWidth() > parent.width) {
+			Logger.log("The width of the help frame is greater than the width of the main frame.");
+		}
+		if (this.frame.getHeight() > parent.height) {
+			Logger.log("The height of the help frame is greater than the height of the main frame.");
+		}
+
+		int xCoordinate = (parent.height / 2) - (this.frame.getWidth() / 2)
+				+ parent.x;
+		int yCoordinate = (parent.height / 2) - (this.frame.getHeight() / 2)
+				+ parent.y;
+		this.frame.setLocation(xCoordinate, yCoordinate);
+
 	}
 
-	// TODO prevent more than one help window from being instantiated at once
+	private void makeFrame() {
+		this.frame = new JFrame();
+		centerOnFrame(this.mainFrame.getBounds(), HelpFrame.PERCENT_OF_WIDTH,
+				HelpFrame.PERCENT_OF_HEIGHT);
+		// TODO use the center on frame to position on the parent
+	}
 }
